af3d3728614eaf2c0970265e0f28d583d905b51d
Athernix <143547367+Athernix@users.noreply.github.com>
2023-10-07 15:18:53 -0500

    test_borde(1)

--- Touch witcher/Assets/Scenes/Scripts/Posiones_Mana.cs
+++ Touch witcher/Assets/Scenes/Scripts/Posiones_Mana.cs
@@ -5,14 +5,5 @@ using UnityEngine;
 public class Posiones_Mana : MonoBehaviour
 {
       
-   private void OnTriggerEnter2D(Collider2D other) {
-		if(other.gameObject.CompareTag("Player"))
-		{
-			bool ManaRecuperado = GameManager.Instance.recuperar_Mana();
-			
-			if(ManaRecuperado) {
-				Destroy(this.gameObject);
-			}
-		}
-	}
+ 
 }
