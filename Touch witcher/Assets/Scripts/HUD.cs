211c6e76bde549f02c1ab2a364f359a719e96070
Athernix <143547367+Athernix@users.noreply.github.com>
2023-11-14 18:44:28 -0500

    Corrección_Menu_v1
    se agregaron scripts para el funcionamiento del menú pero tiene el mismo erro del principió el cual es que se pausa el tiempo al regresar del menú principal.

--- Touch witcher/Assets/Scripts/HUD.cs
+++ Touch witcher/Assets/Scripts/HUD.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 
 public class HUD : MonoBehaviour
@@ -11,12 +12,28 @@ public class HUD : MonoBehaviour
 
     void Update()
     {
-        puntos.text = GameManager.Instance.PuntosTotales.ToString();
+       
     }
 
     public void ActualizarPuntos(int puntosTotales)
     {
         puntos.text = puntosTotales.ToString();
     }
-    
+    [SerializeField] private GameObject botonPausa;
+    [SerializeField] private GameObject menu_Pausa;
+    public void Pausa(){
+            Time.timeScale = 0f;
+            botonPausa.SetActive(false);
+            menu_Pausa.SetActive(true);
+    }
+    public void Renaudar(){
+        Time.timeScale = 1f;
+        botonPausa.SetActive(true);
+        menu_Pausa.SetActive(false);
+        
+
+    }
+    public void salir(){
+        SceneManager.LoadScene(0);
+    }
 }
